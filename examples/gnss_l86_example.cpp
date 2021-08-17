5c6d488c18d32e80d5859633f7b821bad70d541a
Eduardo Ponz Segrelles <eduardoponz@eprosima.com>
2021-08-17 21:24:40 +0200

    Migrate to CMake
    Signed-off-by: Eduardo Ponz Segrelles <eduardoponz@eprosima.com>

--- examples/gnss_l86_example.cpp
+++ examples/gnss_l86_example.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2021 Eduardo Ponz Segrelles.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #include <chrono>
 #include <errno.h>
 #include <iostream>
@@ -15,7 +35,7 @@ void print_position(position position)
     std::cout << "Latitude -------------> " << position.latitude             << std::endl;
     std::cout << "Longitude ------------> " << position.longitude            << std::endl;
     std::cout << "Fix ------------------> " << position.fix                  << std::endl;
-    std::cout << "Number of satelites --> " << position.number_of_satelites  << std::endl;
+    std::cout << "Number of satellites -> " << position.number_of_satelites  << std::endl;
     std::cout << "Horizontal Precision -> " << position.horizontal_precision << std::endl;
     std::cout << "Altitude -------------> " << position.altitude             << std::endl;
 }
@@ -25,7 +45,7 @@ int64_t get_epoch()
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 }
 
-int main() 
+int main()
 {
     GnssInterface gnss;
     std::cout << "GNSS Inteface created!" << std::endl;
